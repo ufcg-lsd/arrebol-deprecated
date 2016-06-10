be9288219b3225df347f66acc593546fa510539a
marcosnobrega <marcosnobregajr@gmail.com>
2016-06-10 09:05:57 -0300

    Changed Arrebol CLI to encode the hash in base64

--- src/main/java/org/fogbowcloud/app/utils/AuthUtils.java
+++ src/main/java/org/fogbowcloud/app/utils/AuthUtils.java
@@ -9,18 +9,24 @@ import org.fogbowcloud.app.model.User;
 
 public class AuthUtils {
 
-	public static int getNonce(){
+	public static int getNonce() {
 		return new Random().nextInt(999999);
 	}
-	
-	public static boolean checkUserInAuthToken(String hash, User user, int nonce) throws IOException, GeneralSecurityException {
+
+	public static boolean checkUserInAuthToken(String hash, User user, int nonce)
+			throws IOException, GeneralSecurityException {
 		if (user == null) {
 			return false;
 		}
-		RSAPrivateKey privateKey = RSAUtils.getPrivateKeyFromString(user.getPrivateKey());
+		// When using the arrebol CLI the generated base64 hash has some line breaks
+		// to be able to send it in the request we replace the new line char with "*"
+		// and here we need to replace it back with the new line char
+		hash = hash.replace("*", "\n");
+		RSAPrivateKey privateKey = RSAUtils
+				.getPrivateKeyFromString(user.getPrivateKey());
 		String decrypt = RSAUtils.decrypt(hash, privateKey);
 		String userInHash = decrypt.replace(String.valueOf(nonce), "");
 		return userInHash.equals(user.getUsername());
-		
+
 	}
 }
