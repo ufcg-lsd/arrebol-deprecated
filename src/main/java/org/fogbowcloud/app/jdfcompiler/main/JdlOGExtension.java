7266f5a64a1bd15c9b243b379db22bfb5a440ef3
IgorVictor <magusdovento@gmail.com>
2016-07-05 12:53:09 -0300

    Added the CommonCompiler and the jdf grammar and files to the project, removed the Ourgrid dependence

---
+++ src/main/java/org/fogbowcloud/app/jdfcompiler/main/JdlOGExtension.java
@@ -0,0 +1,14 @@
+package org.fogbowcloud.app.jdfcompiler.main;
+
+import org.glite.jdl.Jdl;
+
+/**
+ * This class contains attribute extensions for JDL that are used
+ * in Ourgrid.
+ *
+ */
+public class JdlOGExtension extends Jdl {
+	
+    public static final String NAME = "Name";
+
+}
